053221b366e53f6fcdb2da9e9b5b16c467cb7969
leehojin <dlghwls0203@naver.com>
2023-11-19 15:17:41 +0900

    [add] rent_content 액티비티 캘린더 전까지 구현

---
+++ app/src/main/java/com/example/hakbokwe/RentContentActivity.java
@@ -0,0 +1,14 @@
+package com.example.hakbokwe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RentContentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rent_content);
+    }
+}
