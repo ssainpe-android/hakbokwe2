5d52c133bf31dff33f43d9e4b4486d36a5f14612
leehojin <dlghwls0203@naver.com>
2023-11-26 23:02:55 +0900

    feat : drawerlayout으로 메뉴 구성 및 화면 연결

--- app/src/main/java/com/example/hakbokwe/MainActivity.java
+++ app/src/main/java/com/example/hakbokwe/MainActivity.java
@@ -9,23 +9,35 @@ package com.example.hakbokwe;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.example.hakbokwe.databinding.ActivityMainBinding;
 import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
-
+    DrawerLayout drawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
+        //드로우 레이아웃 객체 저장
+        drawer = findViewById(R.id.drawer);
+        //첫번째는 홈화면
         loadFragment(new HomeFragment());
+        //옆에메뉴 리스너
+        drawerlistener();
+        //네비게이션 리스너
+        navigationinit();
+    }
+
+    private void navigationinit() {
         binding.mainBnv.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener(){
 
             @Override
@@ -34,13 +46,29 @@ public class MainActivity extends AppCompatActivity {
                     loadFragment(new MypageFragment());
                 } else if (item.getItemId() == R.id.homeFragment) {
                     loadFragment(new HomeFragment());
+                } else if (item.getItemId() == R.id.menuFragment) {
+                    drawer.open();
                 }
                 return false;
             }
         });
     }
 
+    private void drawerlistener() {
+        binding.drawerMenuAnnouncement.setOnClickListener(v -> {
+            drawer.close();
+            loadFragment(new NoticeListFragment());
+        });
+        binding.drawerMenuAboutus.setOnClickListener(v -> {
+            drawer.close();
+            loadFragment(new AboutusFragment());
+        });
+    }
+
     private void loadFragment(Fragment fragment){
-        getSupportFragmentManager().beginTransaction().replace(R.id.main_frm, fragment).commit();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.main_frm, fragment)
+                .commit();
     }
 }
