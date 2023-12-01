30ff78b8f37c53bb68a8d433df4ec896f1551eaf
leehojin <dlghwls0203@naver.com>
2023-12-01 18:44:19 +0900

    add: 보증금까지 넣어서 rent부분 ㄹㅇ 다 구현 이제 날짜,시간정보만 받으면 된다.

--- app/src/main/java/com/example/hakbokwe/RentAccountActivity.java
+++ app/src/main/java/com/example/hakbokwe/RentAccountActivity.java
@@ -1,20 +1,48 @@
 package com.example.hakbokwe;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.hakbokwe.databinding.ActivityRentAccountBinding;
 
 public class RentAccountActivity extends AppCompatActivity {
+    ActivityRentAccountBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        ActivityRentAccountBinding binding = ActivityRentAccountBinding.inflate(getLayoutInflater());
+        binding = ActivityRentAccountBinding.inflate(getLayoutInflater());
         super.onCreate(savedInstanceState);
         //뒤로가기 버튼
+        back();
+        getdeposit();
+        finishrent();
+        setContentView(binding.getRoot());
+    }
+
+    private void finishrent() {
+        binding.rentAccountLayout.setOnClickListener(v -> {
+            finish();
+            loadFragment(new HomeFragment());
+        });
+    }
+
+    private void back() {
         binding.rentAccountBackBtn.setOnClickListener(v -> {
             finish();
         });
-        setContentView(binding.getRoot());
+    }
+
+    private void getdeposit() {
+        Intent getIntent = getIntent();
+        int deposit = getIntent.getIntExtra("deposit",0);
+        binding.rentAccountPriceTv.setText(String.valueOf(deposit));
+    }
+    private void loadFragment(Fragment fragment){
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.main_frm, fragment)
+                .commit();
     }
 }
--- app/src/main/java/com/example/hakbokwe/RentContentActivity.java
+++ app/src/main/java/com/example/hakbokwe/RentContentActivity.java
@@ -4,28 +4,56 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.CalendarView;
 
+import com.bumptech.glide.Glide;
 import com.example.hakbokwe.databinding.ActivityRentContentBinding;
 
 public class RentContentActivity extends AppCompatActivity {
     ActivityRentContentBinding binding;
     BottomSheetDialogFragment bottomSheetDialogFragment;
+    private int deposit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        bottomSheetDialogFragment = new BottomSheetDialogFragment();
         super.onCreate(savedInstanceState);
         binding = ActivityRentContentBinding.inflate(getLayoutInflater());
         //뒤로가기 버튼
+        back();
+        //리사이클러뷰에서 물품 정보 가져오기
+        getstuff_info();
+        //바텀쉿다이얼로그 띄우기
+        bottomsheet_init();
+        setContentView(binding.getRoot());
+    }
+
+    private void back() {
         binding.rentContentBackBtn.setOnClickListener(v -> {
             finish();
         });
-        //바텀쉿다이얼로그 띄우기
+    }
+
+    private void bottomsheet_init() {
+        bottomSheetDialogFragment = new BottomSheetDialogFragment(deposit);
         binding.rentContentRentalBtn.setOnClickListener(v -> {
             bottomSheetDialogFragment.show(getSupportFragmentManager(),bottomSheetDialogFragment.getTag());
         });
-        setContentView(binding.getRoot());
+    }
+
+    private void getstuff_info() {
+        //인텐트 받기
+        Intent getIntent = getIntent();
+        //받은 정보 저장
+        String name = getIntent.getStringExtra("name");
+        String profile = getIntent.getStringExtra("profile");
+        int quantity = getIntent.getIntExtra("quantity",0);
+        deposit = getIntent.getIntExtra("deposit",0);
+        //받은 정보 바인딩
+        binding.rentContentItemNameTv.setText(name);
+        binding.rentContentItemLeftQuantity.setText(String.valueOf(quantity));
+        binding.rentContentDeposit.setText(String.valueOf(deposit));
+        Glide.with(this).load(profile).into(binding.rentContentItemImageIv);
     }
 
 }
